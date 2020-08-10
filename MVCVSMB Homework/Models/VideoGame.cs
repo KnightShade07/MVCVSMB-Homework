8651f24ad012e6a63611fcb0d8d5b523d8cfb2ab
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-10 11:19:37 -0700

    Created the model with documentation

---
+++ MVCVSMB Homework/Models/VideoGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCVSMB_Homework.Models
+{
+    public class VideoGame
+    {
+        /// <summary>
+        /// The ID of the game given by the Database
+        /// </summary>
+        public int GameID { get; set; }
+
+        /// <summary>
+        /// The name of the game.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The rating of the game given by the ESRB
+        /// </summary>
+
+        public char ESRBRating { get; set; }
+
+        /// <summary>
+        /// The systems that the game is on
+        /// </summary>
+
+        public string Systems { get; set; }
+
+        /// <summary>
+        /// The price of the game.
+        /// </summary>
+        public decimal Price { get; set; }
+
+        
+
+
+    }
+}
