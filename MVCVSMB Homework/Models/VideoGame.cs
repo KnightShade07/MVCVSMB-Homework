f2d5a671707fbbf01c48ea453c817bf1d7ce0884
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-11 19:43:10 -0700

    Implemented Basic Validation and Controller Enchancements!

--- MVCVSMB Homework/Models/VideoGame.cs
+++ MVCVSMB Homework/Models/VideoGame.cs
@@ -16,23 +16,28 @@ namespace MVCVSMB_Homework.Models
         /// <summary>
         /// The name of the game.
         /// </summary>
+        
+        [Required(ErrorMessage ="Please enter a title.")]
         public string Title { get; set; }
 
         /// <summary>
         /// The rating of the game given by the ESRB
         /// </summary>
         [Display(Name ="ESRB Rating: ")]//these dont use ; at the end.
+        [Required(ErrorMessage ="Please enter a rating.")]
         public char ESRBRating { get; set; }
 
         /// <summary>
         /// The systems that the game is on
         /// </summary>
 
+        [Required(ErrorMessage ="Please specify what system the game is on.")]
         public string Systems { get; set; }
 
         /// <summary>
         /// The price of the game.
         /// </summary>
+        [Required(ErrorMessage ="Please enter the price for the game you're looking for.")]
         public decimal Price { get; set; }
 
         
